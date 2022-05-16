771ee95e86cc69a69dbe01ea1704ae315e91aeaf
Daniela-Mera <daniela.antonieta@gmail.com>
2022-05-16 12:13:21 -0400

    con scaffold asignaturas funcionando

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_16_150633) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_16_160817) do
+  create_table "asignaturas", force: :cascade do |t|
+    t.string "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
